91956b0f280465b75ba5f7fa42ac4668220e9148
ma9pie <ma9pie.dev@gmail.com>
2024-05-16 00:05:45 +0900

    fix : build error

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import autoprefixer from 'autoprefixer';
-import cssimport from 'postcss-import';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import tscAlias from 'rollup-plugin-tsc-alias';
 
 import pkg from './package.json';
